2466752ed4dbbb577252b188681eef8784ec8959
hishinuma616 <dnbwoodleaves3616@gmail.com>
2019-01-20 03:13:05 +0900

    update:カレンダー追加

--- components/ShopList.vue
+++ components/ShopList.vue
@@ -19,36 +19,43 @@
             </ul>
           </div>
           <div class='shop-body'>
-            <!-- ディスクリプション -->
-            <div class='l-bottom-midium'>
-              <div class='shop-data'>
-                <div class='shop-copy'>ハイボール100円、プレミアム生350円、京風おでん、炭火焼、鮮魚が自慢。ご予約はお電話で</div>
-              </div>
-              <div>ネット予約で貯まる・使える</div>
-            </div>
 
-            <div class='l-bottom-xsmall'>
-              <div class='shop-rate'>
-                <ul class='shop-rate-star'>
-                  <li><img src='../assets/images/shopList/star-enable.png'></li>
-                  <li><img src='../assets/images/shopList/star-enable.png'></li>
-                  <li><img src='../assets/images/shopList/star-enable.png'></li>
-                  <li><img src='../assets/images/shopList/star-disable.png'></li>
-                  <li><img src='../assets/images/shopList/star-disable.png'></li>
+            <div class='l-flex'>
+              <div>
+                <!-- ディスクリプション -->
+                <div class='l-bottom-midium'>
+                  <div class='shop-data'>
+                    <div class='shop-copy'>ハイボール100円、プレミアム生350円、京風おでん、炭火焼、鮮魚が自慢。ご予約はお電話で</div>
+                  </div>
+                  <div>ネット予約で貯まる・使える</div>
+                </div>
+
+                <div class='l-bottom-xsmall'>
+                  <div class='shop-rate'>
+                    <ul class='shop-rate-star'>
+                      <li><img src='../assets/images/shopList/star-enable.png'></li>
+                      <li><img src='../assets/images/shopList/star-enable.png'></li>
+                      <li><img src='../assets/images/shopList/star-enable.png'></li>
+                      <li><img src='../assets/images/shopList/star-disable.png'></li>
+                      <li><img src='../assets/images/shopList/star-disable.png'></li>
+                    </ul>
+                    <span class='shop-score'>3.52</span>
+                    <span class='shop-reviewCount'><a href=''>66件</a></span>
+                  </div>
+                </div>
+
+                <div class='l-bottom-midium'>
+                  <span class='shop-budget'><span class='shop-budget-night'></span>¥3,000 ~ ¥3,999</span><span class='shop-budget'><span class='shop-budget-morning'></span>-</span>
+                </div>
+
+                <ul class='shop-kwd l-bottom-midium'>
+                  <li>個室</li><li>全席喫煙可</li><li>飲み放題</li><li>クーポン</li>
                 </ul>
-                <span class='shop-score'>3.52</span>
-                <span class='shop-reviewCount'><a href=''>66件</a></span>
+              </div>
+              <div class='shop-tpoint-container'>
+                <a class='shop-tpoint' href=''>ネット予約で貯まる・使える</a>
               </div>
             </div>
-
-            <div class='l-bottom-midium'>
-              <span class='shop-budget'><span class='shop-budget-night'></span>¥3,000 ~ ¥3,999</span><span class='shop-budget'><span class='shop-budget-morning'></span>-</span>
-            </div>
-
-            <ul class='shop-kwd l-bottom-midium'>
-              <li>個室</li><li>全席喫煙可</li><li>飲み放題</li><li>クーポン</li>
-            </ul>
-
             <div class='shop-pickupReview-container'>
               <a class='shop-pickupReview' href=''>
                 <p class='l-bottom-small'><span class='shop-pickupReview-title'>赤坂で絶対に外さない、いや外せないお店</span><span class='shop-pickupReview-writer'>by 白ひげ海賊団(946)</span></p>
@@ -56,6 +63,108 @@
               </a>
             </div>
           </div>
+        </div>
+        <div class='shop-calender-container'>
+          <ul class='shop-calender'>
+            <li class='shop-calender-sun'>
+              <p class='shop-calender-week'>日</p>
+              <div class='shop-calender-body'>
+                <p class='shop-calender-date'>1/20</p>
+                <p class='shop-calender-vacancy'>-</p>
+              </div>
+            </li>
+            <li>
+              <p class='shop-calender-week'>月</p>
+              <div class='shop-calender-body'>
+                <p class='shop-calender-date'>21</p>
+                <p class='shop-calender-vacancy'>-</p>
+              </div>
+            </li>
+            <li>
+              <p class='shop-calender-week'>火</p>
+              <div class='shop-calender-body'>
+                <p class='shop-calender-date'>22</p>
+                <p class='shop-calender-vacancy'>-</p>
+              </div>
+            </li>
+            <li>
+              <p class='shop-calender-week'>水</p>
+              <div class='shop-calender-body'>
+                <p class='shop-calender-date'>23</p>
+                <p class='shop-calender-vacancy'>-</p>
+              </div>
+            </li>
+            <li>
+              <p class='shop-calender-week'>木</p>
+              <div class='shop-calender-body'>
+                <p class='shop-calender-date'>24</p>
+                <p class='shop-calender-vacancy'>-</p>
+              </div>
+            </li>
+            <li>
+              <p class='shop-calender-week'>金</p>
+              <div class='shop-calender-body'>
+                <p class='shop-calender-date'>25</p>
+                <p class='shop-calender-vacancy'>-</p>
+              </div>
+            </li>
+            <li class='shop-calender-sat'>
+              <p class='shop-calender-week'>土</p>
+              <div class='shop-calender-body'>
+                <p class='shop-calender-date'>26</p>
+                <p class='shop-calender-vacancy'>-</p>
+              </div>
+            </li>
+            <li class='shop-calender-sun'>
+              <p class='shop-calender-week'>日</p>
+              <div class='shop-calender-body'>
+                <p class='shop-calender-date'>27</p>
+                <p class='shop-calender-vacancy'>-</p>
+              </div>
+            </li>
+            <li>
+              <p class='shop-calender-week'>月</p>
+              <div class='shop-calender-body'>
+                <p class='shop-calender-date'>28</p>
+                <p class='shop-calender-vacancy'>-</p>
+              </div>
+            </li>
+            <li>
+              <p class='shop-calender-week'>火</p>
+              <div class='shop-calender-body'>
+                <p class='shop-calender-date'>29</p>
+                <p class='shop-calender-vacancy'>-</p>
+              </div>
+            </li>
+            <li>
+              <p class='shop-calender-week'>水</p>
+              <div class='shop-calender-body'>
+                <p class='shop-calender-date'>30</p>
+                <p class='shop-calender-vacancy'>-</p>
+              </div>
+            </li>
+            <li>
+              <p class='shop-calender-week'>木</p>
+              <div class='shop-calender-body'>
+                <p class='shop-calender-date'>31</p>
+                <p class='shop-calender-vacancy'>-</p>
+              </div>
+            </li>
+            <li>
+              <p class='shop-calender-week'>金</p>
+              <div class='shop-calender-body'>
+                <p class='shop-calender-date'>2/1</p>
+                <p class='shop-calender-vacancy'>-</p>
+              </div>
+            </li>
+            <li class='shop-calender-sat'>
+              <p class='shop-calender-week'>土</p>
+              <div class='shop-calender-body'>
+                <p class='shop-calender-date'>2</p>
+                <p class='shop-calender-vacancy'>-</p>
+              </div>
+            </li>
+          </ul>
         </div>
       </li>
     </ul>
@@ -96,9 +205,50 @@
     }
   }
 
+  .shopList-genre {
+    color: #949499;
+    font-size: 10px;
+  }
+
   .shop {
     display: flex;
     padding: 10px 10px 12px;
+
+    .shop-body {
+      margin-right: 18px;
+    }
+
+    .shop-tpoint-container {
+      flex: 0 0 auto;
+      margin-left: 18px;
+      width: 165px;
+      text-align: center;
+
+      .shop-tpoint {
+        display: block;
+        padding: 5px;
+        border: solid 1px #d6d4b4;
+        font-size: 10px;
+        font-weight: bold;
+        letter-spacing: -.5px;
+        color: #665f4e;
+        vertical-align: bottom;
+        line-height: 14px;
+
+        &:hover {
+          text-decoration: none;
+        }
+
+        &::before {
+          content: '';
+          display: inline-block;
+          width: 14px;
+          height: 14px;
+          background: url('~assets/images/tpoint.png');
+          margin-right: 6px;
+        }
+      }
+    }
 
     .shop-img-container {
       margin-right: 12px;
@@ -271,6 +421,63 @@
       .shop-pickupReview-writer {
         font-size: 10px;
       }
+    }
+  }
+
+  .shop-calender-container {
+    margin-bottom: 18px;
+  }
+
+  .shop-calender {
+    display: flex;
+
+    & > li {
+      width: calc(100% / 14);
+
+      & .shop-calender-body {
+        border: solid 1px #e9e9e9;
+
+        &:not(:last-child) {
+          border-right: none !important;
+        }
+      }
+
+      &:not(:last-child) {
+        & .shop-calender-body {
+          border-right: none !important;
+        }
+      }
+    }
+
+    .shop-calender-sun {
+      .shop-calender-week,
+      .shop-calender-date {
+        color: #e64c30;
+      }
+    }
+
+    .shop-calender-sat {
+      .shop-calender-week,
+      .shop-calender-date {
+        color: #0066cc;
+      }
+    }
+
+    .shop-calender-week {
+      text-align: center;
+      padding: 5px;
+    }
+
+    .shop-calender-body {
+      padding: 8px 5px;
+    }
+
+    .shop-calender-date {
+      text-align: center;
+    }
+
+    .shop-calender-vacancy {
+      text-align: center;
     }
   }
 }
