39a6a13f819155ca9f47f905dc3fefc99840ada9
hishinuma616 <dnbwoodleaves3616@gmail.com>
2019-01-21 00:20:20 +0900

    update:shopList完成

--- components/ShopList.vue
+++ components/ShopList.vue
@@ -2,20 +2,26 @@
   <div>
     <ul>
       <li class='shopList'>
-        <div class='shopList-header'>
-          <a class='shopList-name' href=''>赤坂　ごだいご</a>
-          <span class='shopList-genre'>居酒屋、おでん、焼き鳥</span>
+        <div class='l-flex'>
+          <div class='shopList-header'>
+            <a class='shopList-name' href=''>赤坂　ごだいご</a>
+            <span class='shopList-genre'>居酒屋、おでん、焼き鳥</span>
+          </div>
+          <div class='shopList-menu'>
+            <a href=''><div class='shopList-menu-icon shopList-menu-icon-visit'></div></a>
+            <a href=''><div class='shopList-menu-icon shopList-menu-icon-save'></div></a>
+          </div>
         </div>
 
         <div class='shop'>
           <div class='shop-img-container'>
             <div class='shop-img'><img src='../assets/images/shopList/1.jpg'></div>
             <ul class='shop-thumbnail'>
-              <li class='shop-thumbnail-item'><img src='../assets/images/shopList/detail1.jpg'></li>
-              <li class='shop-thumbnail-item'><img src='../assets/images/shopList/detail1.jpg'></li>
-              <li class='shop-thumbnail-item'><img src='../assets/images/shopList/detail1.jpg'></li>
-              <li class='shop-thumbnail-item'><img src='../assets/images/shopList/detail1.jpg'></li>
-              <li class='shop-thumbnail-item'><img src='../assets/images/shopList/detail1.jpg'></li>
+              <li class='shop-thumbnail-item'><img src='../assets/images/shopList/1-1.jpg'></li>
+              <li class='shop-thumbnail-item'><img src='../assets/images/shopList/1-2.jpg'></li>
+              <li class='shop-thumbnail-item'><img src='../assets/images/shopList/1-3.jpg'></li>
+              <li class='shop-thumbnail-item'><img src='../assets/images/shopList/1-4.jpg'></li>
+              <li class='shop-thumbnail-item'><img src='../assets/images/shopList/1-5.jpg'></li>
             </ul>
           </div>
           <div class='shop-body'>
@@ -110,10 +116,10 @@
             </li>
             <li class='shop-calender-sat'>
               <p class='shop-calender-week'>土</p>
-              <div class='shop-calender-body'>
+              <a href='' class='shop-calender-body is-possible'>
                 <p class='shop-calender-date'>26</p>
-                <p class='shop-calender-vacancy'>-</p>
-              </div>
+                <p class='shop-calender-vacancy'><span class='shop-calender-vacancy-little'></span></p>
+              </a>
             </li>
             <li class='shop-calender-sun'>
               <p class='shop-calender-week'>日</p>
@@ -152,17 +158,190 @@
             </li>
             <li>
               <p class='shop-calender-week'>金</p>
-              <div class='shop-calender-body'>
+              <a href='' class='shop-calender-body is-possible'>
                 <p class='shop-calender-date'>2/1</p>
+                <p class='shop-calender-vacancy'><span class='shop-calender-vacancy-enough'></span></p>
+              </a>
+            </li>
+            <li class='shop-calender-sat'>
+              <p class='shop-calender-week'>土</p>
+              <a href='' class='shop-calender-body is-possible'>
+                <p class='shop-calender-date'>2</p>
+                <p class='shop-calender-vacancy'><span class='shop-calender-vacancy-enough'></span></p>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </li>
+
+      <li class='shopList'>
+        <div class='l-flex'>
+          <div class='shopList-header'>
+            <a class='shopList-name' href=''>炭焼き料理・国産酒 wa 円頓寺</a>
+            <span class='shopList-genre'> [愛知] 国際センター駅 381m / 居酒屋、おでん、もつ鍋</span>
+          </div>
+          <div class='shopList-menu'>
+            <a href=''><div class='shopList-menu-icon shopList-menu-icon-visit'></div></a>
+            <a href=''><div class='shopList-menu-icon shopList-menu-icon-save'></div></a>
+          </div>
+        </div>
+
+        <div class='shop'>
+          <div class='shop-img-container'>
+            <div class='shop-img'><img src='../assets/images/shopList/2.jpg'></div>
+            <ul class='shop-thumbnail'>
+              <li class='shop-thumbnail-item'><img src='../assets/images/shopList/2-1.jpg'></li>
+              <li class='shop-thumbnail-item'><img src='../assets/images/shopList/2-2.jpg'></li>
+              <li class='shop-thumbnail-item'><img src='../assets/images/shopList/2-3.jpg'></li>
+              <li class='shop-thumbnail-item'><img src='../assets/images/shopList/2-4.jpg'></li>
+              <li class='shop-thumbnail-item'><img src='../assets/images/shopList/2-5.jpg'></li>
+            </ul>
+          </div>
+          <div class='shop-body'>
+
+            <div class='l-flex'>
+              <div>
+                <!-- ディスクリプション -->
+                <div class='l-bottom-midium'>
+                  <div class='shop-data'>
+                    <div class='shop-copy'> [愛知] 国際センター駅 381m / 居酒屋、おでん、もつ鍋</div>
+                  </div>
+                  <div>ネット予約で貯まる・使える</div>
+                </div>
+
+                <div class='l-bottom-xsmall'>
+                  <div class='shop-rate'>
+                    <ul class='shop-rate-star'>
+                      <li><img src='../assets/images/shopList/star-enable.png'></li>
+                      <li><img src='../assets/images/shopList/star-enable.png'></li>
+                      <li><img src='../assets/images/shopList/star-enable.png'></li>
+                      <li><img src='../assets/images/shopList/star-disable.png'></li>
+                      <li><img src='../assets/images/shopList/star-disable.png'></li>
+                    </ul>
+                    <span class='shop-score'>3.05</span>
+                    <span class='shop-reviewCount'><a href=''>2件</a></span>
+                  </div>
+                </div>
+
+                <div class='l-bottom-midium'>
+                  <span class='shop-budget'><span class='shop-budget-night'></span>¥3,000 ~ ¥3,999</span><span class='shop-budget'><span class='shop-budget-morning'></span>¥3,000 ~ ¥3,999</span>
+                </div>
+
+                <ul class='shop-kwd l-bottom-midium'>
+                  <li>個室</li><li>全席喫煙可</li><li>飲み放題</li><li>クーポン</li>
+                </ul>
+              </div>
+              <div class='shop-tpoint-container'>
+                <a class='shop-tpoint' href=''>ネット予約で貯まる・使える</a>
+              </div>
+            </div>
+            <div class='shop-pickupReview-container'>
+              <a class='shop-pickupReview' href=''>
+                <p class='l-bottom-small'><span class='shop-pickupReview-title'>15時から昼飲みが出来て、お店の雰囲気も良い居酒屋さん。</span><span class='shop-pickupReview-writer'>by イク蔵(157)</span></p>
+                <p>12月23日（土）16時30分頃の訪問。名古屋駅から円頓寺方面に歩いて帰る道すがら、雰...</p>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class='shop-calender-container'>
+          <ul class='shop-calender'>
+            <li class='shop-calender-sun'>
+              <p class='shop-calender-week'>日</p>
+              <div class='shop-calender-body'>
+                <p class='shop-calender-date'>1/20</p>
+                <p class='shop-calender-vacancy'>-</p>
+              </div>
+            </li>
+            <li>
+              <p class='shop-calender-week'>月</p>
+              <div class='shop-calender-body'>
+                <p class='shop-calender-date'>21</p>
+                <p class='shop-calender-vacancy'>-</p>
+              </div>
+            </li>
+            <li>
+              <p class='shop-calender-week'>火</p>
+              <div class='shop-calender-body'>
+                <p class='shop-calender-date'>22</p>
+                <p class='shop-calender-vacancy'>-</p>
+              </div>
+            </li>
+            <li>
+              <p class='shop-calender-week'>水</p>
+              <div class='shop-calender-body'>
+                <p class='shop-calender-date'>23</p>
+                <p class='shop-calender-vacancy'>-</p>
+              </div>
+            </li>
+            <li>
+              <p class='shop-calender-week'>木</p>
+              <div class='shop-calender-body'>
+                <p class='shop-calender-date'>24</p>
+                <p class='shop-calender-vacancy'>-</p>
+              </div>
+            </li>
+            <li>
+              <p class='shop-calender-week'>金</p>
+              <div class='shop-calender-body'>
+                <p class='shop-calender-date'>25</p>
                 <p class='shop-calender-vacancy'>-</p>
               </div>
             </li>
             <li class='shop-calender-sat'>
               <p class='shop-calender-week'>土</p>
+              <a href='' class='shop-calender-body is-possible'>
+                <p class='shop-calender-date'>26</p>
+                <p class='shop-calender-vacancy'><span class='shop-calender-vacancy-little'></span></p>
+              </a>
+            </li>
+            <li class='shop-calender-sun'>
+              <p class='shop-calender-week'>日</p>
               <div class='shop-calender-body'>
-                <p class='shop-calender-date'>2</p>
+                <p class='shop-calender-date'>27</p>
                 <p class='shop-calender-vacancy'>-</p>
               </div>
+            </li>
+            <li>
+              <p class='shop-calender-week'>月</p>
+              <div class='shop-calender-body'>
+                <p class='shop-calender-date'>28</p>
+                <p class='shop-calender-vacancy'>-</p>
+              </div>
+            </li>
+            <li>
+              <p class='shop-calender-week'>火</p>
+              <div class='shop-calender-body'>
+                <p class='shop-calender-date'>29</p>
+                <p class='shop-calender-vacancy'>-</p>
+              </div>
+            </li>
+            <li>
+              <p class='shop-calender-week'>水</p>
+              <a href='' class='shop-calender-body is-possible'>
+                <p class='shop-calender-date'>30</p>
+                <p class='shop-calender-vacancy'><span class='shop-calender-vacancy-enough'></span></p>
+              </a>
+            </li>
+            <li>
+              <p class='shop-calender-week'>木</p>
+              <a href='' class='shop-calender-body is-possible'>
+                <p class='shop-calender-date'>31</p>
+                <p class='shop-calender-vacancy'><span class='shop-calender-vacancy-enough'></span></p>
+              </a>
+            </li>
+            <li>
+              <p class='shop-calender-week'>金</p>
+              <a href='' class='shop-calender-body is-possible'>
+                <p class='shop-calender-date'>2/1</p>
+                <p class='shop-calender-vacancy'><span class='shop-calender-vacancy-enough'></span></p>
+              </a>
+            </li>
+            <li class='shop-calender-sat'>
+              <p class='shop-calender-week'>土</p>
+              <a href='' class='shop-calender-body is-possible'>
+                <p class='shop-calender-date'>2</p>
+                <p class='shop-calender-vacancy'><span class='shop-calender-vacancy-enough'></span></p>
+              </a>
             </li>
           </ul>
         </div>
@@ -181,8 +360,46 @@
   }
 
   .shopList-header {
+    width: 640px;
     padding: 13px 10px;
     border-bottom: solid 1px #e1e1e1;
+  }
+
+  .shopList-menu {
+    display: flex;
+    justify-content: flex-end;
+    flex: 1 1 auto;
+    border-bottom: solid 1px #e1e1e1;
+
+    & > * {
+      position: relative;
+      width: 50px;
+      margin: 10px 0;
+      text-align: center;
+    }
+
+    & > *:not(:last-child) {
+      border-right: solid 1px #e1e1e1;
+    }
+
+    .shopList-menu-icon {
+      position: absolute;
+      top: calc(50% - 9px);
+      left: calc(50% - 9px);
+      content: '';
+      display: inline-block;
+      width: 18px;
+      height: 18px;
+      padding: 5px 0;
+
+      &.shopList-menu-icon-visit {
+        background: url('~assets/images/visit.png');
+      }
+
+      &.shopList-menu-icon-save {
+        background: url('~assets/images/save.png');
+      }
+    }
   }
 
   .shopList-name {
@@ -213,10 +430,6 @@
   .shop {
     display: flex;
     padding: 10px 10px 12px;
-
-    .shop-body {
-      margin-right: 18px;
-    }
 
     .shop-tpoint-container {
       flex: 0 0 auto;
@@ -251,7 +464,7 @@
     }
 
     .shop-img-container {
-      margin-right: 12px;
+      margin-right: 6px;
 
       .shop-img {
         border: solid 1px #e1e1e1;
@@ -341,7 +554,7 @@
     }
 
     .shop-budget {
-      font-size: 12px;
+      font-size: 10px;
 
       &:not(:last-child) {
         margin-right: 9px;
@@ -384,6 +597,7 @@
         color: #797152;
         border: solid 1px #d6ceb4;
         padding: 4px;
+        font-size: 10px;
 
         &:not(:last-child) {
           margin-right: 5px;
@@ -470,14 +684,49 @@
 
     .shop-calender-body {
       padding: 8px 5px;
+
+      &.is-possible {
+        display: block;
+        &:hover {
+          background: #faf8f5;
+          text-decoration: none;
+        }
+      }
     }
 
     .shop-calender-date {
       text-align: center;
+      font-size: 14px;
+      line-height: 1.4;
+      margin-bottom: 3px;
+      color: #13131e;
     }
 
     .shop-calender-vacancy {
       text-align: center;
+      color: #b4b4b4;
+
+      .shop-calender-vacancy-little {
+        &::before {
+          content: '';
+          display: inline-block;
+          background: url('~assets/images/triangle.png');
+          background-size: contain;
+          width: 13px;
+          height: 13px;
+        }
+      }
+
+      .shop-calender-vacancy-enough {
+        &::before {
+          content: '';
+          display: inline-block;
+          background: url('~assets/images/circle.png');
+          background-size: contain;
+          width: 13px;
+          height: 13px;
+        }
+      }
     }
   }
 }
